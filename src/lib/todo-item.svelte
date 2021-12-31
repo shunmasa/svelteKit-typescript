4c62dea932eccdd21fcafd15c5a10542f44343b7
masashi suka <masashilandjob@gmail.com>
2021-12-31 01:50:33 +0000

    Add Patch endpoint - mark as done

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let todo:Todo;
-
+  // const done = todo.done;
 </script>
 
 
@@ -65,7 +65,7 @@
     opacity: 1;
   }
   
-  /* .done {
+   .done {
     transform: none;
     opacity: 0.4;
     filter: drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.1));
@@ -73,14 +73,16 @@
   
   .done .toggle {
     background-image: url("data:image/svg+xml,%3Csvg width='22' height='16' viewBox='0 0 22 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 1.5L7.4375 14.5L1.5 8.5909' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
-  } */
+  } 
 
 </style>
-
-<div class="todo">
- <form action=""method="">
-     <input type="hidden" name="done" value="" />
-     <button aria-label="MArk done/not done"class="toggle">Done/NotDone</button>
+<!-- conditionally done is true , automatically class done is peioritised
+class:done = {todo.done} but if const done = todo.done => only class:done 
+-->
+<div class="todo" class:done = {todo.done}>
+ <form action="/todos/{todo.uid}.json?_method=patch" method="post">
+     <input type="hidden" name="done" value="{todo.done ? '' : 'true'}" />
+     <button aria-label="Mark todo as {todo.done ? 'not done' : 'done'}"class="toggle">Done/NotDone</button>
 
  </form>
  
--- src/routes/todos/_api.ts
+++ src/routes/todos/_api.ts
@@ -34,7 +34,8 @@ export const api = (request:Request,data?:Record<string,unknown>) =>{
     case "PATCH"://update
         todos = todos.map(todo =>{
            if(todo.uid === request.params.uid){
-           todo.text = data.text as string;
+           if(data.text) todo.text = data.text as string;
+           else todo.done = data.done as boolean;
            }
            return todo
        })
