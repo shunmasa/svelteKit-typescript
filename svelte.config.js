250187c159b13522b14247a2c7e9b52763aae28a
masashi suka <masashilandjob@gmail.com>
2021-12-31 22:09:46 +0000

    Configure vercel-build && postbuild

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
+//$ ecport VERCEL=true
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
